5b72ef7444fd4c9e37743ecb493784aba5cb2cd3
Melanie Murray <melaniemurr13@gmail.com>
2025-05-07 14:34:21 -0600

    Practiced with user data types

---
+++ UserDataTypes/UserDatatypes.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+enum Meat {
+    Chicken,
+    Prawns,
+    Pastrami
+};
+
+class Sandwich {
+public:
+    std::string bread;
+    bool isDelicious;
+    Meat meat;
+
+private:
+    float price;
+
+public:
+    void setPrice(float value) {
+        price = value;
+    }
+    void PrintPrice(Sandwich sandwich) {
+        std::cout << "Price: $" << sandwich.price << std::endl;
+    }
+  };
+
+
+int main() {
+    //typedef float salary; -- traditional C++ way
+    using salary = float; // modern C++ way
+    salary wage_butcher = 12.4f;
+    salary wage_cleaner = 9;
+    salary wage_chef = 15;
+
+    Sandwich bbq;
+
+    bbq.bread = "Honey Rye";
+    //bbq.price = 22; -- Price is private now
+    bbq.setPrice(29.98f);
+    bbq.isDelicious = true;
+
+    bbq.meat = Meat::Prawns;
+
+    //PrintPrice(bbq); -- Procedural Programming = member functions separate from objects
+    bbq.PrintPrice(); // OOP = data members + member functions
+  }
