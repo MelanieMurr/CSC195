640d7bf58023c803865d09b474a58a9486949a45
Melanie Murray <melaniemurr13@gmail.com>
2025-05-07 12:05:33 -0600

    Staged variables to project

---
+++ DataTypes/DataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    char initial;
+    short age;
+    bool isAdult;
+    unsigned int zipcode;
+    float wage;
+    short daysWorked;
+    float hoursWorkedPerDay;
+    const float TAX = 0.1f;
+}
