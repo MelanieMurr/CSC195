ffe3e8a4850d860afaeb949ab16cdef3eb1f470d
Melanie Murray <melaniemurr13@gmail.com>
2025-05-07 12:25:30 -0600

    Added ability to grab user input, hours worked

--- DataTypes/DataTypes.cpp
+++ DataTypes/DataTypes.cpp
@@ -3,12 +3,33 @@ using namespace std;
 
 int main() {
     string name;
+    cout << "Enter first name: ";
+    cin >> name;
     char initial;
+    cout << "Enter initial of last name: ";
+    cin >> initial;
     short age;
+    cout << "Enter age: ";
+    cin >> age;
     bool isAdult;
+    isAdult = (age >= 18);
     unsigned int zipcode;
+    cout << "Enter zipcode: ";
+    cin >> zipcode;
     float wage;
+    cout << "Enter hourly wage: ";
+    cin >> wage;
     short daysWorked;
-    float hoursWorkedPerDay;
+    cout << "Enter number of days worked (max 7): ";
+    cin >> daysWorked;
+    float hoursWorkedPerDay[7];
     const float TAX = 0.1f;
+
+    float totalHours = 0;
+    for (int i = 0; i < daysWorked; i++) {
+        cout << "Enter hours worked for the day " << (i + 1) << ": ";
+        cin >> hoursWorkedPerDay[i];
+
+        totalHours += hoursWorkedPerDay[i];
+    }
 }
